36c3f1e1a508af29b09c55ca4947819e5b2205f0
Eric Roberts <eric.roberts@mapmyfitnessinc.com>
2019-03-07 13:26:50 -0600

    spit out user-ip mappings

--- mysql/mysql_ec2_clients.sh
+++ mysql/mysql_ec2_clients.sh
@@ -18,18 +18,32 @@ MYSQL_HOST=$1
 MYSQL_USER=$2
 AWS_PROFILE=${3:default}
 
+TMP_FILE=/tmp/mysql_raw.txt
+
 #
 # Get IPs of hosts connected to MySQL server
 #
-ips=$(mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASS \
+mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASS \
      -e "SELECT concat(HOST,',',USER) as output FROM INFORMATION_SCHEMA.PROCESSLIST ORDER BY HOST" | \
-     awk -F ',' '{split($1,ip,":"); print ip[1], $2}' | sort | uniq | sort | \
-     awk '{print $1}' | egrep -v '(localhost|output)' | tr '\n' ',' | sed 's/,$//')
+     awk -F ',' '{split($1,ip,":"); print $2, ip[1]}' | sort | uniq | sort > $TMP_FILE
+
+echo ""
+echo "Users connected to $MYSQL_HOST by IP:"
+echo ""
+cat $TMP_FILE
+
+ips=$(cat $TMP_FILE | awk '{print $2}' | egrep -v 'output' | tr '\n' ',' | sed 's/,$//')
 
 #
 # Lookup EC2 host information for those IPs
 #
+echo ""
+echo "EC2 nodes and their IPs connected to $MYSQL_HOST:"
+echo ""
+
 aws --profile $AWS_PROFILE ec2 describe-instances \
     --filters "Name=private-ip-address,Values=$ips" | 
     jq -r '.Reservations[].Instances[0] | (.Tags[]|select(.Key=="Name")|.Value) as $name | "\($name) \(.PrivateIpAddress)"' | \
     column  -t | sort
+
+rm $TMP_FILE
